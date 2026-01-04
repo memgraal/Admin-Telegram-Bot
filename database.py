e2d08f54dd5380fcc8b9dc308897628544710476
Ovsianko Andrew <ovsiankoandrew@MacBook-Pro-Ovsianko.local>
2026-01-04 12:47:31 +0300

    plural fix

--- database.py
+++ database.py
@@ -13,7 +13,7 @@ class Base(AsyncAttrs, DeclarativeBase):
     pass
 
 
-class GroupUsers(Base):
+class GroupUser(Base):
     __tablename__ = "group_users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -27,8 +27,8 @@ class GroupUsers(Base):
 
     status: Mapped[str] = mapped_column(String(32))
 
-    user: Mapped["Users"] = relationship(back_populates="groups")
-    group: Mapped["Groups"] = relationship(back_populates="users")
+    user: Mapped["User"] = relationship(back_populates="groups")
+    group: Mapped["Group"] = relationship(back_populates="users")
 
     def __repr__(self):
         return (
@@ -37,13 +37,13 @@ class GroupUsers(Base):
         )
 
 
-class Users(Base):
+class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[str] = mapped_column(unique=True)
 
-    groups: Mapped[list["GroupUsers"]] = relationship(
+    groups: Mapped[list["GroupUser"]] = relationship(
         back_populates="user",
         cascade="all, delete-orphan"
     )
@@ -52,14 +52,14 @@ class Users(Base):
         return f"User(id={self.id}, user_id={self.user_id})"
 
 
-class Groups(Base):
+class Group(Base):
     __tablename__ = "groups"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     chat_id: Mapped[str] = mapped_column(unique=True)
     settings: Mapped[JSON] = mapped_column(type_=JSON)
 
-    users: Mapped[list["GroupUsers"]] = relationship(
+    users: Mapped[list["GroupUser"]] = relationship(
         back_populates="group",
         cascade="all, delete-orphan"
     )
